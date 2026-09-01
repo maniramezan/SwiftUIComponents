74eba3697e5c2f703dc18f38566aa0076d8bac24
maniramezan <mani.ramezan@gmail.com>
2026-09-01 00:19:20 -0400

    Exercise action pill body in tests

--- Tests/SwiftUIComponentsTests/Components/ActionPillTests.swift
+++ Tests/SwiftUIComponentsTests/Components/ActionPillTests.swift
@@ -5,10 +5,11 @@ import Testing
 @Test("ActionPill accepts dynamic label content")
 @MainActor
 func actionPillAcceptsDynamicLabelContent() {
-    _ = ActionPill(action: {}) {
+    let pill = ActionPill(action: {}) {
         HStack {
             Text("past")
             Text("walked")
         }
     }
+    _ = pill.body
 }
